12383f90dae69c5558fe9b7a35727aae10aa3f59
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2024-02-11 12:28:48 +0600

    transition to home page

--- lib/pages/leading_page.dart
+++ lib/pages/leading_page.dart
@@ -1,7 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-class LeadingPage extends StatelessWidget {
+import 'package:taskify/pages/home_page.dart';
+class LeadingPage extends StatefulWidget {
   const LeadingPage({super.key});
 
+  @override
+  State<LeadingPage> createState() => _LeadingPageState();
+}
+
+class _LeadingPageState extends State<LeadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2), (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+    }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
